e81ad3babca9059dba491dceeca6c2b62074dfeb
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2025-04-23 09:44:47 +0530

    cp.c

--- Assign1/cp.c
+++ Assign1/cp.c
@@ -3,29 +3,27 @@
 #include<sys/types.h> 
 #include<sys/stat.h> 
 #include<fcntl.h> 
-#include<unistd.h> 
- 
+#include<unistd.h>
+
 int main(int argc, char* argv[]) { 
-if(argc != 3) { 
+if(argc != 3) {
 printf("You can not enter more or less than 3 arguments."); 
-return 0; 
-} 
+return 0;
+}
 char* fn1; 
 char* fn2; 
 char line[50]; 
-int fp1; 
+int fp1;
 int fp2; 
-int n; 
+int n;
 fn1 = argv[1]; 
-fn2 = argv[2]; 
+fn2 = argv[2];
 fp1 = open(fn1, O_RDONLY); 
-fp2 = open(fn2, O_WRONLY); 
+fp2 = open(fn2, O_WRONLY);
 while ((n = read(fp1, line, 50)) > 0) { 
-write(fp2, line, n); 
-} 
+write(fp2, line, n);
+}
 close(fp2); 
 close(fp1); 
-return 0; 
-} 
- 
-  
+return 0;
+}
